d92f4aba13d3704795ea426465aff777fe051939
Joel Almeida <almeidaalajoel@gmail.com>
2022-08-13 18:54:29 -0700

    named import D:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "../globals.css";
 import { Layout } from "../components";
-import { Script } from "next/script";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
