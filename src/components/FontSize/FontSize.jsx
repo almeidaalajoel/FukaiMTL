e8de16899e18eabbf5c6835b7736a56ad73ee8fd
Joel <almeidaalajoel@gmail.com>
2022-08-15 13:14:14 -0700

    fixed nonjsx svg

--- src/components/FontSize/FontSize.jsx
+++ src/components/FontSize/FontSize.jsx
@@ -120,11 +120,11 @@ export default function FontSize({
           color={size === "4xl" ? "gray" : "black"}
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
