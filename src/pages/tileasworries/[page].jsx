0ff65652e1451735eebf00f92a124faae7702309
Joel <almeidaalajoel@gmail.com>
2022-08-15 23:59:37 -0700

    fixed apostrophes in html tilea chapter page

--- src/pages/tileasworries/[page].jsx
+++ src/pages/tileasworries/[page].jsx
@@ -140,7 +140,7 @@ export default function TileasWorries() {
     <div className="flex flex-col items-center dark:bg-black">
       <div className="flex flex-grow flex-col w-full lg:w-[60rem] bg-white dark:bg-[rgb(23,21,21)] dark:text-white p-4 lg:p-12 lg:pt-6 leading-7 text-xl border border-solid border-gray-300 dark:border-gray-900">
         <p className="self-center text-3xl text-[#282c34] dark:text-white select-none">
-          Tilea's Worries
+          Tilea&apos;s Worries
         </p>
         <Navigation
           TOClink="/tileasworries"
