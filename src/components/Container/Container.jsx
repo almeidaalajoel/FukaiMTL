8f7f5b304b43f16f26f786aa9857a3236ef1e0d2
Joel <almeidaalajoel@gmail.com>
2022-08-19 15:57:42 -0700

    added ads

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Script from "next/script";
 
 export default function Container({ children }) {
   return (
-    <div className="flex flex-col flex-grow items-center bg-[rgb(230,230,230)] dark:bg-black">
-      {children}
+    <div className="flex flex-row h-full justify-around bg-[rgb(230,230,230)] dark:bg-black">
+      <span className="w-[20rem] text-white bg-[rgb(230,230,230)] dark:bg-black">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3890555720020555"
+          crossorigin="anonymous"
+        ></Script>
+        <ins
+          class="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-3890555720020555"
+          data-ad-slot="1149041230"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <Script>(adsbygoogle = window.adsbygoogle || []).push({});</Script>
+      </span>
+      <div className="flex flex-col items-center">{children}</div>
+      <span className="w-[20rem] text-white bg-[rgb(230,230,230)] dark:bg-black">
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3890555720020555"
+          crossorigin="anonymous"
+        ></Script>
+        <ins
+          class="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-3890555720020555"
+          data-ad-slot="1149041230"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <Script>(adsbygoogle = window.adsbygoogle || []).push({});</Script>
+      </span>
     </div>
   );
 }
--- src/components/InnerContainer/InnerContainer.jsx
+++ src/components/InnerContainer/InnerContainer.jsx
@@ -1,9 +1,24 @@
+import Script from "next/script";
 import React from "react";
 
 export default function InnerContainer({ children }) {
   return (
     <div className="flex bg-white flex-grow w-full flex-col pt-6 items-center lg:w-[73rem] space-y-4 pb-16 dark:bg-[rgb(21,19,19)] dark:text-white">
       {children}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3890555720020555"
+        crossorigin="anonymous"
+      ></Script>
+      <ins
+        class="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-3890555720020555"
+        data-ad-slot="1787900138"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <Script>(adsbygoogle = window.adsbygoogle || []).push({});</Script>
     </div>
   );
 }
