85e9aa448a30a83f47063610919eb573271855a1
Joel <almeidaalajoel@gmail.com>
2022-08-19 17:34:14 -0700

    crossorigin->crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3890555720020555"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </Head>
       <body>
