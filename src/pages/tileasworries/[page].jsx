b2bde7c89c5bb0002da270bb61850df7747c830a
Joel <almeidaalajoel@gmail.com>
2022-08-23 13:47:57 -0700

    less padding on mobile

--- src/pages/tileasworries/[page].jsx
+++ src/pages/tileasworries/[page].jsx
@@ -130,7 +130,7 @@ export default function TileasWorries({ dark }) {
 
   return ready ? (
     <Container>
-      <div className="flex flex-grow flex-col w-full lg:w-[65%] xl:w-[53%] text-[rgb(10,10,10)] bg-white dark:bg-[rgb(23,21,21)] dark:text-[rgb(200,200,200)] p-4 lg:p-12 lg:pt-6 leading-7 text-xl border border-solid border-gray-300 dark:border-gray-900">
+      <div className="flex flex-grow flex-col w-full lg:w-[65%] xl:w-[53%] text-[rgb(10,10,10)] bg-white dark:bg-[rgb(23,21,21)] dark:text-[rgb(200,200,200)] p-2 lg:p-12 lg:pt-6 leading-7 text-xl border border-solid border-gray-300 dark:border-gray-900">
         <p className="self-center text-3xl text-[#282c34] dark:text-white select-none">
           Tilea&apos;s Worries
         </p>
