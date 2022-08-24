6962c6f0005caa97883e8dc0d6b600a481892fdb
Joel <almeidaalajoel@gmail.com>
2022-08-23 19:51:35 -0700

    padding on buttons

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Navigation({ TOClink, prevURL, nextURL }) {
   const nextPrev =
-    "flex border-solid border-4 border-[rgb(70,70,70)] hover:border-black hover:text-black dark:border-gray-300  dark:hover:border-white dark:hover:text-white dark:hover:bg-[rgb(28,25,25)] w-1/4 h-[3rem] rounded-2xl items-center justify-center select-none hover:cursor-pointer";
+    "flex px-4 border-solid border-4 border-[rgb(70,70,70)] hover:border-black hover:text-black dark:border-gray-300  dark:hover:border-white dark:hover:text-white dark:hover:bg-[rgb(28,25,25)] w-1/4 h-[3rem] rounded-2xl items-center justify-center select-none hover:cursor-pointer";
 
   const noNextPrev =
-    "flex border-solid border-4 border-[rgb(180,180,180)] text-[rgb(180,180,180)] dark:border-gray-700 dark:text-gray-700  w-1/4 h-[3rem] rounded-2xl items-center justify-center select-none";
+    "flex px-4 border-solid border-4 border-[rgb(180,180,180)] text-[rgb(180,180,180)] dark:border-gray-700 dark:text-gray-700  w-1/4 h-[3rem] rounded-2xl items-center justify-center select-none";
 
   const prevInnards = (
     <>
