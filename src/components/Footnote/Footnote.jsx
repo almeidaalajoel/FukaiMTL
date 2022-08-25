59cec917a895eac6bd7b85d5d9b0bd3e1ceb2f08
Joel <almeidaalajoel@gmail.com>
2022-08-24 17:07:50 -0700

    footnote scales with text size, text still black on noveltile in dark mode

--- src/components/Footnote/Footnote.jsx
+++ src/components/Footnote/Footnote.jsx
@@ -22,7 +22,7 @@ export default function Footnote({ num, note, start, end, footnotes }) {
         onClick={() => setShowNote(false)}
         className={
           showNote
-            ? "mx-auto break-words border-solid border border-green-100 w-4/5 text-[smaller] shadow-lg py-1 px-2 rounded-md bg-stone-100 h-min min-h-[3rem] my-2 hover:cursor-pointer dark:bg-stone-700 dark:border-green-900"
+            ? "mx-auto break-words border-solid border border-green-100 w-4/5 text-[smaller] shadow-lg py-1 px-2 rounded-md bg-stone-100 h-min min-h-[2.5em] my-2 hover:cursor-pointer dark:bg-stone-700 dark:border-green-900"
             : "hidden"
         }
       >
--- src/components/NovelTile/NovelTile.jsx
+++ src/components/NovelTile/NovelTile.jsx
@@ -6,7 +6,7 @@ export default function NovelTile({ title, description, image, author, link }) {
   return (
     <Link href={link}>
       <div className="flex flex-col w-[24rem] flex-grow px-4 hover:cursor-pointer font-['ProximaNova'] leading-6 rounded-2xl items-center sm:flex-row  sm:justify-evenly sm:w-[38rem] md:w-[50rem] lg:w-full dark:bg-[rgb(21,19,19)]">
-        <div className="flex flex-col items-center justify-evenly p-3 w-[17rem] border-2 border-solid bg-[rgb(230,240,240)] border-black  rounded-xl mb-5 sm:mb-0 sm:mr-4">
+        <div className="flex flex-col items-center justify-evenly p-3 w-[17rem] border-2 border-solid bg-[rgb(230,240,240)] border-black text-[#282c34] rounded-xl mb-5 sm:mb-0 sm:mr-4">
           <div className="w-[14rem] relative mb-3">
             <Image
               alt={title}
@@ -15,8 +15,8 @@ export default function NovelTile({ title, description, image, author, link }) {
               className="rounded-xl max-h-full max-w-full "
             />
           </div>
-          <p className="text-[#282c34] text-[1.75rem]">{title}</p>
-          <div className="">by {author}</div>
+          <p className="text-[1.75rem]">{title}</p>
+          <p>by {author}</p>
         </div>
         <div className="flex flex-col justify-center sm:w-[20rem] md:w-[26rem] lg:w-[32rem] dark:text-white">
           {description}
