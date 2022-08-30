7d7f3b0d9c2b5787087a38ebde6577a84bac159d
almeidaalajoel <30331921+almeidaalajoel@users.noreply.github.com>
2022-08-29 20:51:10 -0700

    css solution to mobile hover

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,16 +2,14 @@ import React, { useRef } from "react";
 import Link from "next/link";
 
 export default function Navigation({ TOClink, prevURL, nextURL }) {
-  const prevRef = useRef();
-
   const nextPrev =
-    "flex px-4 lg:px-6 py-2 border-solid border-4 border-[rgb(70,70,70)] hover:border-black hover:text-black dark:bg-[rgb(23,21,21)] dark:border-gray-300  dark:hover:border-white dark:hover:text-white bg-[rgb(250,250,250)] hover:bg-white dark:hover:bg-[rgb(31,27,27)] rounded-2xl items-center justify-center select-none hover:cursor-pointer";
+    "flex px-4 lg:px-6 py-2 border-solid border-4 border-[rgb(70,70,70)] canHov:hover:border-black canHov:hover:text-black dark:bg-[rgb(23,21,21)] canHov:hover:bg-white dark:border-gray-300  dark:canHov:hover:border-white dark:canHov:hover:text-white bg-[rgb(250,250,250)] dark:canHov:hover:bg-[rgb(31,27,27)] rounded-2xl items-center justify-center select-none hover:cursor-pointer";
 
   const noNextPrev =
     "flex px-4 lg:px-6 py-2 border-solid border-4 border-[rgb(180,180,180)] text-[rgb(180,180,180)] dark:border-gray-700 dark:text-gray-700 rounded-2xl items-center justify-center select-none";
 
   const prevInnards = (
-    <>
+    <React.Fragment>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -27,11 +25,11 @@ export default function Navigation({ TOClink, prevURL, nextURL }) {
         />
       </svg>
       PREV
-    </>
+    </React.Fragment>
   );
 
   const nextInnards = (
-    <>
+    <React.Fragment>
       NEXT
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +45,7 @@ export default function Navigation({ TOClink, prevURL, nextURL }) {
           d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
         />
       </svg>
-    </>
+    </React.Fragment>
   );
 
   return (
@@ -56,7 +54,7 @@ export default function Navigation({ TOClink, prevURL, nextURL }) {
         {prevURL === "no" ? (
           <div className={noNextPrev}>{prevInnards}</div>
         ) : (
-          <Link ref={prevRef} onClick={prevRef.current.blur()} href={prevURL}>
+          <Link href={prevURL}>
             <div className={nextPrev}>{prevInnards}</div>
           </Link>
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
+      canHov: { raw: "(hover: hover)" },
+
       smPh: "390px",
 
       mdPh: "470px",
